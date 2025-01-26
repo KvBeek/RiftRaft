d20457933f9afd7a05cd186178cb89fe9c3d3920
KvBeek <111359691+KvBeek@users.noreply.github.com>
2025-01-26 17:11:13 +0100

    Bouying calculatie afgemaakt
    De vier bouyings worden nu op de juiste positie gezet in de hoeken.

--- OceanRiftRaft/Assets/Scripts/Water/BouyingSetPositions.cs
+++ OceanRiftRaft/Assets/Scripts/Water/BouyingSetPositions.cs
@@ -1,34 +1,39 @@
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class BouyingSetPositions : MonoBehaviour
 {
     [SerializeField] GameObject[] buoyancies = new GameObject[4];
     [SerializeField] LayerMask layerMask;
 
-    public void Update()
-    {
-        float right = Ray(Vector3.right).x;
-        float left = Ray(Vector3.left).x;
-        float forward = Ray(Vector3.forward).y;
-        float back = Ray(Vector3.back).y;
+    void Start(){
 
-        Debug.DrawRay(new Vector2(right,back), Vector3.up, Color.red);
-        Debug.DrawRay(new Vector2(right,forward), Vector3.up, Color.green);
-        
-        Debug.DrawRay(new Vector2(left,back), Vector3.up, Color.blue);
-        Debug.DrawRay(new Vector2(left,forward), Vector3.up, Color.yellow);
     }
 
-    Vector3 Ray(Vector3 right)
+    public void CalcBouyingPosition()
+    {
+        Vector3 right = Ray(Vector3.right);
+        Vector3 left = Ray(Vector3.left);
+        Vector3 forward = Ray(Vector3.forward);
+        Vector3 back = Ray(Vector3.back);
+
+        // Debug.DrawRay(new Vector3(right.x, 0,forward.z), Vector3.up, Color.red);  
+        // Debug.DrawRay(new Vector3(right.x, 0,back.z), Vector3.up, Color.red);  
+        // Debug.DrawRay(new Vector3(left.x, 0,forward.z), Vector3.up, Color.red);  
+        // Debug.DrawRay(new Vector3(left.x, 0,back.z), Vector3.up, Color.red);  
+
+        buoyancies[0].transform.position =  new Vector3(right.x, buoyancies[0].transform.position.y,forward.z);
+        buoyancies[1].transform.position =  new Vector3(right.x, buoyancies[1].transform.position.y,back.z);
+        buoyancies[2].transform.position =  new Vector3(left.x, buoyancies[2].transform.position.y,forward.z);
+        buoyancies[3].transform.position =  new Vector3(left.x, buoyancies[3].transform.position.y,back.z);
+    }
+
+    Vector3 Ray(Vector3 dir)
     {
         RaycastHit hitx;
 
-        Vector3 start = transform.position + transform.TransformDirection(right) * 100;
-        Vector3 end = transform.TransformDirection(right) * -1;
-
+        Vector3 start = transform.position + transform.TransformDirection(dir) * 100;
+        Vector3 end = transform.TransformDirection(dir) * -1;
         Physics.Raycast(start, end, out hitx, Mathf.Infinity, layerMask);
-        Debug.DrawRay(start, end * hitx.distance, Color.yellow);
 
         return hitx.point;
     }
